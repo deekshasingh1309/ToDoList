0a80c52982c77ea9ef62d9ad069cc0fa6e61b673
deekshasingh1309 <47428380+deekshasingh1309@users.noreply.github.com>
2019-02-28 03:49:24 +0530

    Update to_do_list.js

--- to_do_list.js
+++ to_do_list.js
@@ -1,85 +1,90 @@
 
+function createinput() {
+    var body = document.getElementsByTagName("body")[0];
+    var container = document.createElement("container")
+    container.id = "con"
+    var input = document.createElement("input")
+    input.type = "date"
+    input.name = "dateinput"
+    input.id = "date"
+    input.appendChild(document.createTextNode("dates"))
+    container.appendChild(input)
 
-//function removeItem(){
-//	var ul = document.getElementById("dynamic-list");
-  //var candidate = document.getElementById("candidate");
-  //var item = document.getElementById(candidate.value);
-  //ul.removeChild(item);
-//}
+    var input1 = document.createElement("input");
+    input1.type = "text"
+    input1.name = "desc"
+    input1.id = "descript"
+    input1.placeholder = "Description"
+    input1.appendChild(document.createTextNode("dsc"))
+    container.appendChild(input1);
+    var btn = document.createElement("button")
+    btnvalue = document.createTextNode("OK")
+    btn.onclick = ok
+    btn.appendChild(btnvalue)
+    container.append(btn)
+
+    var btn1 = document.createElement("button")
+    btn1value = document.createTextNode("CANCEL")
+    btn1.onclick = cancel
+    btn1.appendChild(btn1value)
+    container.appendChild(btn1)
 
 
-function createinput()
-    { 
-        var body=document.getElementsByTagName("body")[0];
-        var container=document.createElement("container")
-        container.id="con"
-        var input = document.createElement("input")
-        input.type = "date"
-        input.name = "dateinput"
-        input.id="date"
-        input.appendChild(document.createTextNode("dates"))
-        container.appendChild(input)
-
-        var input1 = document.createElement("input");
-        input1.type = "text"
-        input1.name = "desc"   
-        input1.id="descript"  
-        input1.placeholder="Description"
-        input1.appendChild(document.createTextNode("dsc")) 
-        container.appendChild(input1);
-        var btn=document.createElement("button")
-        btnvalue=document.createTextNode("OK")
-        btn.onclick=ok
-        btn.appendChild(btnvalue)
-        container.append(btn)
-
-        var btn1=document.createElement("button")
-        btn1value=document.createTextNode("CANCEL")
-        btn1.onclick=cancel
-        btn1.appendChild(btn1value)
-        container.appendChild(btn1)
-        
-    
-        console.log(container)
-        body.appendChild(container)
-    }
-    var data=[
-        {
-            "date1":  "20/01/17",
-            "desc1": "01/01/1900"
-        }
-    ]
-
-    function ok()
+    console.log(container)
+    body.appendChild(container)
+}
+var data = [
     {
-        var dt=document.getElementById("date").value;
-        var ds=document.getElementById("descript").value;
-            if(dt=="" || ds==""){
-                alert("empty");
-            }
-            
-            else{
-                data.push({"date":dt, "desc":ds})
-                createlist();
-            }
+        "date": "20-01-17",
+        "desc": "Project was done"
     }
+]
 
-        function createlist(){
-          var ul = document.createElement("dynamic-list");
-          var  li= document.createElement("li");
-          console.log("hi",data);
-          li.setAttribute('date',data.value);
-          li.setAttribute('des',data.value);
-          li.appendChild(document.createTextNode(data.value));
-          li.appendChild(document.createTextNode(data.value));
-          ul.appendChild(li);
-          document.body.appendChild(ul)
-        }
+function ok() {
+    var dt = document.getElementById("date").value;
+    var ds = document.getElementById("descript").value;
+    if (dt == "" || ds == "") {
+        alert("empty");
+    } else {
+        data.push({ "date": dt, "desc": ds })
+        createlist();
+    }
+}
 
-
-
-    function cancel(){
-
+function createlist() {
+    var df = data;
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.id = "listdiv"
+        var ul = document.createElement("ul");
+        var li = document.createElement("li");
+        var span = document.createElement("span");
+        var span1 = document.createElement("span");
+        span.innerHTML = df[i].date;
+        span1.innerHTML = df[i].desc;
+        li.appendChild(span);
+        li.appendChild(span1);                                          
+        var removebtn = document.createElement("button");
+        removebtn.setAttribute("class", "glyphicon glyphicon-remove");
+        li.appendChild(removebtn)
+        ul.appendChild(li);
+        div.appendChild(ul);
+        removebtn.onclick = remove;
 
     }
+    document.body.appendChild(div);
+}
+
+function cancel() {
+    document.getElementById('con').remove();
+
+
+}
+
+function remove() {
+
+    document.getElementById('listdiv').remove()
+}
+
+
 
